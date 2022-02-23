7ca7f03486277b119983b984e4da7e25cad5bad6
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2022-02-23 18:35:54 +0100

    API calls done

---
+++ js/details.js
@@ -0,0 +1,43 @@
+// 2. Fetch a single result using an id, name, or other property
+
+const oneBurgerHTML = document.querySelector(".burgerresults");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+console.log(id);
+
+const urlTwo = "https://burgers1.p.rapidapi.com/burgers/" + id;
+const options = { "headers": {
+    "x-rapidapi-key": "78bd844603msha9b1f2a81715f8ep181884jsn2834618f18d0",
+}};
+
+console.log(urlTwo);
+
+
+async function getOneBurger() {
+
+    try {
+        const response = await fetch(urlTwo, options);
+        const json = await response.json();
+
+        console.log(json);
+
+        oneBurgerHTML.innerHTML += `<div><h1> ${json.name}</h1>
+                                  <p>You can find this tasty burger at the <a href="${json.web}" class="restaurant-link">${json.restaurant}</a> restaurant</p>
+                                  <p>Description: ${json.description}</p>
+                                  <p>Ingredients: ${json.ingredients}</p></div>` // need space!!!
+
+    } catch (error) {
+        oneBurgerHTML.innerHTML += `An error has occured, please return to main page or contact us`;
+    }
+
+}
+
+getOneBurger()
+
+
+
---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+// FIND AN API
+
+// 1. Fetch an array of results and display at least 3 different properties inside the HTML:
+
+
+const url = "https://burgers1.p.rapidapi.com/burgers";
+const options = { "headers": {
+    "x-rapidapi-key": "78bd844603msha9b1f2a81715f8ep181884jsn2834618f18d0",
+}};
+
+const resultsHTML = document.querySelector(".resultscontainer");
+
+async function getBurgers() {
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        console.log(data);
+
+        resultsHTML.innerHTML = " ";
+
+        for (let i =0; i< data.length; i++){
+            // let results = data[i];
+            // console.log(data[i])
+
+            if (i === 15) {
+                break;
+            }
+
+            if (data[i].web.startsWith("www")) {
+                continue;
+            }
+
+            if (data[i].description === "N/A") {
+                continue;
+            }
+
+
+        resultsHTML.innerHTML += `<a href="/details.html?id=${data[i].id}">
+        <div class="result">
+        <p>Burger: ${data[i].name}</p>
+        <p>Restaurant: ${data[i].restaurant}</p>
+        <p>Description: ${data[i].description}</p>
+        </div>
+        </a>`
+    }
+
+    } catch (error) {
+        resultsHTML.innerHTML += `An error has occured, please try again or contact us`;
+    }
+}
+
+getBurgers()
+
+
+
+
+
